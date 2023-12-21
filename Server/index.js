d8ccd9af4dff5dc7256808244f478f627883b722
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2023-12-22 01:54:48 +0600

    added cors middelware

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 
 
 const app = express();
+
+// middle ware 
+
+app.use(cors())
+
 const port = process.env.port || 5000;
 
 app.get('/',(req,res)=>{
-    res.send('Task-manager server is running')
+    res.status(304).send('Task-manager server is running')
 })
 
 
