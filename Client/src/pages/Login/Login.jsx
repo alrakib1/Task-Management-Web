6d2ac92757e50e0ff052da2001d43b33883cffca
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2023-12-24 05:10:26 +0600

    designed login page

--- Client/src/pages/Login/Login.jsx
+++ Client/src/pages/Login/Login.jsx
@@ -3,11 +3,16 @@ import animation from "../../assets/animations/loginanimation.json";
 import Container from "../../components/shared/Container/Container";
 import useAuth from "../../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { Helmet } from "react-helmet-async";
 
+// icons
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+
 const Login = () => {
+  const [showPass, setShowPass] = useState(false);
+
   const navigate = useNavigate();
   const { logIn, user } = useAuth();
 
@@ -30,9 +35,9 @@ const Login = () => {
   //     })
   //     .catch((error) => {
   //       // console.log(error);
-      
+
   //      error &&  toast.error("Wrong email or Password !!!!");
-    
+
   //     });
   // };
   return (
@@ -40,24 +45,78 @@ const Login = () => {
       <Helmet>
         <title>Task Manager | Login</title>
       </Helmet>
-      {/* <div className="flex flex-col md:flex-row justify-center items-center">
-      
-        <div className="md:w-1/2">
-          <Lottie
-            loop
-            animationData={animation}
-            play
-            className="object-contain"
-          />
+
+      <div>
+        <div className="bg-gray-50 font-Montserrat">
+          {/* login container */}
+
+          <div className="bg-gray-100 flex  rounded-2xl shadow-lg max-w-3xl xs:py-4 py-0 sm:p-5">
+            {/* form */}
+            <div className="sm:w-1/2 px-8">
+              <h1 className="font-bold text-2xl text-blue-900 text-center">
+                Login
+              </h1>
+              <p className="text-sm mt-4 text-blue-900 font-medium">
+                If you are already a member, easily log in
+              </p>
+              <form className="flex flex-col gap-4">
+                <input
+                  className="p-2 mt-8 border rounded-xl"
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                />
+                <div className="relative">
+                  <input
+                    className="p-2 border rounded-xl w-full"
+                    type={showPass ? "text" : "password"}
+                    name="password"
+                    id=""
+                    placeholder="Password"
+                  />
+                  {showPass ? (
+                    <FaRegEyeSlash
+                      className="absolute top-1/2 right-3 cursor-pointer -translate-y-1/2 text-gray-500"
+                      onClick={() => setShowPass((prev) => setShowPass(!prev))}
+                    />
+                  )
+                :
+                (
+                  <FaRegEye
+                    className="absolute top-1/2 right-3 cursor-pointer -translate-y-1/2 text-gray-500"
+                    onClick={() => setShowPass((prev) => setShowPass(!prev))}
+                  />
+                )
+                
+                }
+                </div>
+                <button className="rounded-xl text-white bg-[#004080] py-2">
+                  Login
+                </button>
+              </form>
+              <div className="mt-10 grid grid-cols-3 text-gray-500 items-center">
+                <hr className="border-gray-500" />
+                <p className="text-center text-sm">OR</p>
+                <hr className="border-gray-500" />
+              </div>
+              <div>
+                <button className="border bg-white py-2 w-full rounded-xl">Login in with google</button>
+              </div>
+            </div>
+
+            {/* image */}
+
+            <div className="md:w-1/2  sm:block hidden">
+              <Lottie
+                loop
+                animationData={animation}
+                play
+                className="rounded-2xl bg-[#ADD8E6]"
+              />
+            </div>
+          </div>
         </div>
-    
-
-      </div> */}
-     <div>
-      login
-
-      
-     </div>
+      </div>
     </Container>
   );
 };
