0f4e7cc4fc5d3c64ff470b6e59712a7322ef4bf4
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2023-12-24 08:17:14 +0600

    user add operation created

--- Server/index.js
+++ Server/index.js
@@ -1,23 +1,21 @@
-require('dotenv').config();
-const cors = require('cors');
-const express = require('express');
-
-
+require("dotenv").config();
+const express = require("express");
 const app = express();
-
-// middle ware 
-
-app.use(cors())
-
-const port = process.env.port || 5000;
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 
 
-app.get('/',(req,res)=>{
-    res.status(304).send('Task-manager server is running')
-})
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// middle ware
+
+app.use(cors());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = process.env.url;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,16 +24,35 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    
+    const myDB = client.db("task-manager-DB");
+    const usersCollection = myDB.collection("users");
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
+    
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res
+      .status(201)
+      .send({ message: "user has been added", success: true, result });
+    });
+    
+    app.get('/users', async(req,res)=>{
+      const result = await usersCollection.find().toArray();;
+      res.send(result);
+    })
+    
+    console.log("connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -43,13 +60,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Task-manager server is running");
+});
 
-
-
-
-
-
-app.listen(port,()=>{
-    console.log(`server running on port : ${port}`)
-})
-
+app.listen(port, () => {
+  console.log(`server running on http://localhost:${port}`);
+});
