8a977e8ee632090b434aa47cf68c29cb8c2742f6
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2023-12-24 13:22:38 +0600

    added create todo api

--- Server/index.js
+++ Server/index.js
@@ -4,8 +4,6 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-
-
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -14,10 +12,8 @@ app.use(express.json());
 
 app.use(cors());
 
-
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = process.env.url;
-
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -29,32 +25,47 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
- 
     await client.connect();
-    
+
     const myDB = client.db("task-manager-DB");
     const usersCollection = myDB.collection("users");
-    
- 
+    const todoCollection = myDB.collection("to-do");
+
     await client.db("admin").command({ ping: 1 });
-    
-    
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
       res
-      .status(201)
-      .send({ message: "user has been added", success: true, result });
+        .status(201)
+        .send({ message: "user has been added", success: true, result });
     });
-    
-    app.get('/users', async(req,res)=>{
+
+    app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
-    })
-    
+    });
+
+    // post todo
+
+    app.post("/todo", async (req, res) => {
+      const todo = req.body;
+      const result = await todoCollection.insertOne(todo);
+      res
+        .status(201)
+        .send({ message: "todo has been added", success: true, result });
+    });
+
+    app.get("/todo", async (req, res) => {
+      const result = await todoCollection.find().toString();
+      res.send(result);
+    });
+
+
+
+
     console.log("connected to MongoDB!");
   } finally {
-   
   }
 }
 run().catch(console.dir);
